2a05d34f08083837d64c15327f862f0715c155f2
gyurim <bhd00@naver.com>
2020-04-20 18:00:02 +0900

    스무번째 수업

--- javafxapp/src/co/yedam/app/test/ddds.java
+++ javafxapp/src/co/yedam/app/test/ddds.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class ddds {
 
-public static void main(String[] args) {
-	int money,tag,vat,change;
+	public static void main(String[] args) {
+		int money, tag, vat, change;
 
-Scanner input = new Scanner(System.in);
-System.out.print("받은돈: ");
-money=input.nextInt();
+		Scanner input = new Scanner(System.in);
+		System.out.print("받은돈: ");
+		money = input.nextInt();
 
-System.out.print("상품의총액: "); 
-tag=input.nextInt();
+		System.out.print("상품의총액: ");
+		tag = input.nextInt();
 
-vat=tag/10; // 상품종액에서10%의세금을포함한다고가정하자
-change=money-tag;
-System.out.println("부가세: "+vat);
-System.out.println("잔돈: "+ change);
+		vat = tag / 10; // 상품종액에서10%의세금을포함한다고가정하자
+		change = money - tag;
+		System.out.println("부가세: " + vat);
+		System.out.println("잔돈: " + change);
 
-}
+	}
 }
